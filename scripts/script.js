246950b9542eb62af84f84dad764d2dd5f40b5e0
CelticWizard <35495540+CelticWizard@users.noreply.github.com>
2022-06-26 12:50:28 +0100

    Basic Hamburger

---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+function toggleHamburger(){
+    console.log("Function Executed");
+    let nav = document.getElementById("menu");
+    console.log("Menu selected");
+    nav.classList.toggle("hamToggle");
+}
